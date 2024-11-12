f4bf6c9401cab338c88f1a7e1c82d1372c66108a
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-12 16:55:35 +0900

    스프링부트

--- 7. 스프링 부트/exam05/src/main/java/org/koreait/global/libs/Utils.java
+++ 7. 스프링 부트/exam05/src/main/java/org/koreait/global/libs/Utils.java
@@ -1,4 +1,11 @@
 package org.koreait.global.libs;
 
-public class Utils {
+import org.springframework.stereotype.Component;
+
+// 빈의 이름을 정해줌
+@Component("ut")
+public class Utils { // 빈의 이름을 설정해주기 전의 이름은 utils
+    public String hello(String name) {
+        return String.format("%s님 반갑습니다.",name);
+    }
 }
