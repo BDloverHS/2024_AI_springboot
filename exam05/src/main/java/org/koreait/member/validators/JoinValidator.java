479076cd2dc65f70dadd2ac453e46f8dd2c810a4
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-14 10:50:52 +0900

    스프링부트

--- exam05/src/main/java/org/koreait/member/validators/JoinValidator.java
+++ exam05/src/main/java/org/koreait/member/validators/JoinValidator.java
@@ -1,5 +1,36 @@
 package org.koreait.member.validators;
 
-public class JoinValidator {
+import org.koreait.member.controllers.RequestJoin;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
+@Component
+public class JoinValidator implements Validator {
+    /**
+     * 검증을 할 커맨드 객체를 한정
+     * RequestJoin 커맨드 객체만을 검증할 거야!
+     *
+     * @param clazz the {@link Class} that this {@link Validator} is
+     * being asked if it can {@link #validate(Object, Errors) validate}
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(RequestJoin.class);
+    }
+
+    /**
+     * 커맨드 객체 검증 수행
+     *
+     * Object target : 검증할 커맨드 객체
+     * Errors errors
+     *   - 검증 실패 시 전달할 메세지를 설정
+     * @param target the object that is to be validated
+     * @param errors contextual state about the validation process
+     */
+    @Override
+    public void validate(Object target, Errors errors) {
+        RequestJoin form = (RequestJoin)target;
+    }
 }
