d930a4508577924e6e93550ca468551b9509d1d6
BDloverHS <gustn020702naver.com@gmail.com>
2024-11-27 00:11:16 +0900

    스프링부트

--- exam05/src/main/java/org/koreait/global/scheduling/TestJob.java
+++
@@ -1,4 +0,0 @@
-package org.koreait.global.scheduling;
-
-public class TestJob {
-}
--- exam05/src/main/java/org/koreait/scheduling/TestJob.java
+++ exam05/src/main/java/org/koreait/scheduling/TestJob.java
@@ -1,12 +1,13 @@
 package org.koreait.scheduling;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class TestJob {
-
+    //                                      딜레이 기준을 초로 설정
     //@Scheduled(fixedRate = 3L, timeUnit = TimeUnit.SECONDS) // 3초마다 실행
     public void testScheduling() {
       log.info("3초 마다 실행!");
